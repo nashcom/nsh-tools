08a7aaa4b46d8294f3f8acecd0e6eef95ffb3ca4
Daniel Nashed <nsh@nashcom.de>
2025-08-01 12:52:18 +0000

    moved nshmailx to it's own repository -> https://github.com/nashcom/nshmailx

--- nshmailx/build_alpine_static.sh
+++
@@ -1,1 +0,0 @@
-docker run --rm -v $(pwd):/src -w /src alpine:latest sh -c "apk add --no-cache g++ make musl-dev rapidjson-dev curl-dev openssl-dev openssl-libs-static && cd /src && make -f makefile_alpine_static"
